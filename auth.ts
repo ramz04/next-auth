e1ae58ff7cd4a01a5562a8c745efd7b2a9572f97
Abdul-Rahman Wunpini <wunpini95@gmail.com>
2024-08-01 11:58:09 +0000

    update

--- auth.ts
+++ auth.ts
@@ -1,7 +1,18 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "./lib/db"
+import { getUserById } from "@/data/user"
+import { UserRole } from "@prisma/client"
+
+//Not Working
+// declare module "@auth/core" {
+//   interface session {
+//     user: {
+//       role: "ADMIN" | "USER"
+//     } & DefaultSession["user"]
+//   }
+// }
 
 export const {
   handlers: { GET, POST },
@@ -11,14 +22,24 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ session, token }) {
-      console.log({ sessionToken: token, session })
+      console.log({
+        sessionToken: token,
+      })
       if (token.sub && session.user) {
         session.user.id = token.sub
       }
-      console.log({ session })
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
       return session
     },
     async jwt({ token }) {
+      if (!token.sub) return token
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+      token.role = existingUser.role
       return token
     },
   },
